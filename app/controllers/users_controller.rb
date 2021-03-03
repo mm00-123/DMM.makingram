dff78c40e3be6adc887d720121584b93b1ee8656
mm00-123 <zuotengyui1@gmail.com>
2021-03-03 08:21:46 +0000

    少しレイアウトした　まだ機能残ってる

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:page]).reverse_order
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    params.require(:user).permit(:name, :profile_image)
+  end
+
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0c9b8ae35155f14e38a92cc6342eaee1071560576f6f842cf9b7ca62dad9187f4770efc76455bdfee82f5512f515a0edb3ed1768da1acca9d9dbd9c22bbec7fc'
